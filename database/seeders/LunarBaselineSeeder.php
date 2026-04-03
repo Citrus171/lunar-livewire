569b9e625b1bebd653e93e10481f6704a7699a64
mk <user@ubu>
2026-04-04 01:59:03 +0900

    fix(seeder): update LunarBaselineSeeder for improved data seeding consistency

--- database/seeders/LunarBaselineSeeder.php
+++ database/seeders/LunarBaselineSeeder.php
@@ -18,161 +18,161 @@ use Lunar\Models\TaxClass;
 
 class LunarBaselineSeeder extends Seeder
 {
-    /**
-     * Seed baseline Lunar data needed by project seeders.
-     */
-    public function run(): void
-    {
-        Channel::query()->firstOrCreate(
-            ['handle' => 'webstore'],
-            [
-                'name' => 'Webstore',
-                'default' => true,
-                'url' => 'http://localhost',
-            ]
-        );
+  /**
+   * Seed baseline Lunar data needed by project seeders.
+   */
+  public function run(): void
+  {
+    Channel::query()->firstOrCreate(
+      ['handle' => 'webstore'],
+      [
+        'name' => 'Webstore',
+        'default' => true,
+        'url' => 'http://localhost',
+      ]
+    );
 
-        Language::query()->firstOrCreate(
-            ['code' => 'en'],
-            [
-                'name' => 'English',
-                'default' => true,
-            ]
-        );
+    Language::query()->firstOrCreate(
+      ['code' => 'en'],
+      [
+        'name' => 'English',
+        'default' => true,
+      ]
+    );
 
-        Currency::query()->firstOrCreate(
-            ['code' => 'USD'],
-            [
-                'name' => 'US Dollar',
-                'exchange_rate' => 1,
-                'decimal_places' => 2,
-                'default' => true,
-                'enabled' => true,
-            ]
-        );
+    Currency::query()->firstOrCreate(
+      ['code' => 'USD'],
+      [
+        'name' => 'US Dollar',
+        'exchange_rate' => 1,
+        'decimal_places' => 2,
+        'default' => true,
+        'enabled' => true,
+      ]
+    );
 
-        CustomerGroup::query()->firstOrCreate(
-            ['handle' => 'retail'],
-            [
-                'name' => 'Retail',
-                'default' => true,
-            ]
-        );
+    CustomerGroup::query()->firstOrCreate(
+      ['handle' => 'retail'],
+      [
+        'name' => 'Retail',
+        'default' => true,
+      ]
+    );
 
-        CollectionGroup::query()->firstOrCreate(
-            ['handle' => 'main'],
-            [
-                'name' => 'Main',
-            ]
-        );
+    CollectionGroup::query()->firstOrCreate(
+      ['handle' => 'main'],
+      [
+        'name' => 'Main',
+      ]
+    );
 
-        TaxClass::query()->firstOrCreate(
-            ['name' => 'Default Tax Class'],
-            [
-                'default' => true,
-            ]
-        );
+    TaxClass::query()->firstOrCreate(
+      ['name' => 'Default Tax Class'],
+      [
+        'default' => true,
+      ]
+    );
 
-        $productAttributeGroup = AttributeGroup::query()->firstOrCreate(
-            [
-                'attributable_type' => Product::morphName(),
-                'handle' => 'details',
-            ],
-            [
-                'name' => collect(['en' => 'Details']),
-                'position' => 1,
-            ]
-        );
+    $productAttributeGroup = AttributeGroup::query()->firstOrCreate(
+      [
+        'attributable_type' => Product::morphName(),
+        'handle' => 'details',
+      ],
+      [
+        'name' => collect(['en' => 'Details']),
+        'position' => 1,
+      ]
+    );
 
-        $collectionAttributeGroup = AttributeGroup::query()->firstOrCreate(
-            [
-                'attributable_type' => Collection::morphName(),
-                'handle' => 'collection_details',
-            ],
-            [
-                'name' => collect(['en' => 'Details']),
-                'position' => 1,
-            ]
-        );
+    $collectionAttributeGroup = AttributeGroup::query()->firstOrCreate(
+      [
+        'attributable_type' => Collection::morphName(),
+        'handle' => 'collection_details',
+      ],
+      [
+        'name' => collect(['en' => 'Details']),
+        'position' => 1,
+      ]
+    );
 
-        Attribute::query()->firstOrCreate(
-            [
-                'attribute_type' => Product::morphName(),
-                'handle' => 'name',
-            ],
-            [
-                'attribute_group_id' => $productAttributeGroup->id,
-                'position' => 1,
-                'name' => ['en' => 'Name'],
-                'section' => 'main',
-                'type' => TranslatedText::class,
-                'required' => true,
-                'system' => true,
-                'description' => ['en' => ''],
-                'configuration' => ['richtext' => false],
-            ]
-        );
+    Attribute::query()->firstOrCreate(
+      [
+        'attribute_type' => Product::morphName(),
+        'handle' => 'name',
+      ],
+      [
+        'attribute_group_id' => $productAttributeGroup->id,
+        'position' => 1,
+        'name' => ['en' => 'Name'],
+        'section' => 'main',
+        'type' => TranslatedText::class,
+        'required' => true,
+        'system' => true,
+        'description' => ['en' => ''],
+        'configuration' => ['richtext' => false],
+      ]
+    );
 
-        Attribute::query()->firstOrCreate(
-            [
-                'attribute_type' => Product::morphName(),
-                'handle' => 'description',
-            ],
-            [
-                'attribute_group_id' => $productAttributeGroup->id,
-                'position' => 2,
-                'name' => ['en' => 'Description'],
-                'section' => 'main',
-                'type' => TranslatedText::class,
-                'required' => false,
-                'system' => false,
-                'description' => ['en' => ''],
-                'configuration' => ['richtext' => true],
-            ]
-        );
+    Attribute::query()->firstOrCreate(
+      [
+        'attribute_type' => Product::morphName(),
+        'handle' => 'description',
+      ],
+      [
+        'attribute_group_id' => $productAttributeGroup->id,
+        'position' => 2,
+        'name' => ['en' => 'Description'],
+        'section' => 'main',
+        'type' => TranslatedText::class,
+        'required' => false,
+        'system' => false,
+        'description' => ['en' => ''],
+        'configuration' => ['richtext' => true],
+      ]
+    );
 
-        Attribute::query()->firstOrCreate(
-            [
-                'attribute_type' => Collection::morphName(),
-                'handle' => 'name',
-            ],
-            [
-                'attribute_group_id' => $collectionAttributeGroup->id,
-                'position' => 1,
-                'name' => ['en' => 'Name'],
-                'section' => 'main',
-                'type' => TranslatedText::class,
-                'required' => true,
-                'system' => true,
-                'description' => ['en' => ''],
-                'configuration' => ['richtext' => false],
-            ]
-        );
+    Attribute::query()->firstOrCreate(
+      [
+        'attribute_type' => Collection::morphName(),
+        'handle' => 'name',
+      ],
+      [
+        'attribute_group_id' => $collectionAttributeGroup->id,
+        'position' => 1,
+        'name' => ['en' => 'Name'],
+        'section' => 'main',
+        'type' => TranslatedText::class,
+        'required' => true,
+        'system' => true,
+        'description' => ['en' => ''],
+        'configuration' => ['richtext' => false],
+      ]
+    );
 
-        Attribute::query()->firstOrCreate(
-            [
-                'attribute_type' => Collection::morphName(),
-                'handle' => 'description',
-            ],
-            [
-                'attribute_group_id' => $collectionAttributeGroup->id,
-                'position' => 2,
-                'name' => ['en' => 'Description'],
-                'section' => 'main',
-                'type' => TranslatedText::class,
-                'required' => false,
-                'system' => false,
-                'description' => ['en' => ''],
-                'configuration' => ['richtext' => true],
-            ]
-        );
+    Attribute::query()->firstOrCreate(
+      [
+        'attribute_type' => Collection::morphName(),
+        'handle' => 'description',
+      ],
+      [
+        'attribute_group_id' => $collectionAttributeGroup->id,
+        'position' => 2,
+        'name' => ['en' => 'Description'],
+        'section' => 'main',
+        'type' => TranslatedText::class,
+        'required' => false,
+        'system' => false,
+        'description' => ['en' => ''],
+        'configuration' => ['richtext' => true],
+      ]
+    );
 
-        $productType = ProductType::query()->firstOrCreate(['name' => 'Stock']);
+    $productType = ProductType::query()->firstOrCreate(['name' => 'Stock']);
 
-        $productAttributeIds = Attribute::query()
-            ->where('attribute_type', Product::morphName())
-            ->pluck('id');
+    $productAttributeIds = Attribute::query()
+      ->where('attribute_type', Product::morphName())
+      ->pluck('id');
 
-        $productType->mappedAttributes()->syncWithoutDetaching($productAttributeIds);
-    }
+    $productType->mappedAttributes()->syncWithoutDetaching($productAttributeIds);
+  }
 }
